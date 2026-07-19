c91573407ce18cd1ac96f30faaa7a167e840221c
ID-Inyang <bobbyinyang9@gmail.com>
2026-07-19 15:30:03 +0100

    add registerUser function to handle user registration

---
+++ controllers/authController.js
@@ -0,0 +1,15 @@
+import User from "../models/user"
+import Role from '../models/role'
+
+exports.registerUser = (req, res) => {
+    const { username, password, role } = req.body;
+    const user = new User({ username, role });
+
+    User.register(user, password, (err) => {
+        if (err) {
+            console.log(err);
+            return res.status(500).json({ error: err.message })
+        }
+        res.json({ message: 'User registered successfully' })
+    });
+};
